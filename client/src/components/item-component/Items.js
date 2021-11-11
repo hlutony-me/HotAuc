f4df6b14c1914a89b5d231a35f5c148841493fa6
Olivia Thomas <oliviathomas637@gmail.com>
2021-11-11 17:09:07 -0500

    Item Component
    Signed-off-by: Olivia Thomas <oliviathomas637@gmail.com>

--- client/src/components/item-component/Items.js
+++ client/src/components/item-component/Items.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./Items.css";
 
 function Items() {
-  return <div className="items">items </div>;
+  return (
+    <div className="card">
+      <img src="https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" />
+      <div className="card-body">
+        <h2>Item Name</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam.
+        </p>
+        <h5>Owner Name</h5>
+      </div>
+    </div>
+  );
 }
 
 export default Items;
