fbb15e089155515313fb218f786d732c565c6143
hanhan9334 <lhtony9334@126.com>
2021-11-12 15:11:35 -0500

    conflict resolved

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
 
-function Navbar() {
-    return (
-        <nav className="navbar">
-            Nav
-        </nav>
-    )
+const NavBar = () => (
+  <header className="navbar">
+    <div className="navbar__title navbar__item">HotAuc</div>
+    <div className="navbar__item">Home</div>
+    <div className="navbar__item">Login</div>
+    <div className="navbar__item">Register</div>
+    <div className="navbar__item">Contact Us</div>
+  </header>
+);
+
+function Navbar1() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar1;
