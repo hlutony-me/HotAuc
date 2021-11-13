b401e817ed17ba395950001370ba795f3ad50582
constantlyTiTi <mycentryting@gmail.com>
2021-11-12 20:27:17 -0500

    login/registration/logout

---
+++ client/src/ConstantValue.js
@@ -0,0 +1,3 @@
+export const SERVER_URL="/api/"
+export const LOGIN_INFO_EMPTY_ERROR="Please fill all fields.";
+export const LOGIN_MATCH="match";
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -110,6 +110,7 @@ router.post(
 			}
 
 			const isMatch = await bcrypt.compare(password, user.password)
+          
 			if (!isMatch) {
 				return res
 					.status(400)
