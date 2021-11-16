2a33c6ad80b63ee294eab32115b1f6abe2763cf1
hanhan9334 <lhtony9334@126.com>
2021-11-16 11:57:52 -0500

    id endpoint

--- routes/api/item.js
+++ routes/api/item.js
@@ -4,53 +4,54 @@ const router = express.Router()
 const auth = require("../../middleware/auth")
 const AuctionItem = require("../../models/auction_item")
 
-
 //@route   GET api/item/all
 //@desc    Test route
 //@access  Public
-router.get('/all',async(req, res) => {
-    //res.json({message: "Hello item"});
+router.get("/all", async (req, res) => {
+	//res.json({message: "Hello item"});
 	try {
-        const items = await AuctionItem.find();
-        //const items = await AuctionItem.find({title: /a/ });
-		res.json(items)        
+		const items = await AuctionItem.find()
+		//const items = await AuctionItem.find({title: /a/ });
+		res.json(items)
 	} catch (err) {
 		console.log(err.message)
 		res.status(500).json({ msg: "Auction Item search error - all" })
 	}
-});
+})
 
 //@route   GET api/item/search
 //@desc    Test route
 //@access  Public
-router.get('/search',async(req, res) => {
-    //res.json({message: "Hello item"});
+router.get("/search", async (req, res) => {
+	//res.json({message: "Hello item"});
 	//const { title } = req.body
-    var title = req.query.title	
+	var title = req.query.title
 	//var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
 	try {
-        //const items = await AuctionItem.find(condition);
-        const items = await AuctionItem.find({ title: { $regex: new RegExp(title), $options: "i" }});
-		res.json(items)        
-        //res.json({message: "Hello id " + title});
+		//const items = await AuctionItem.find(condition);
+		const items = await AuctionItem.find({
+			title: { $regex: new RegExp(title), $options: "i" }
+		})
+		res.json(items)
+		//res.json({message: "Hello id " + title});
 	} catch (err) {
 		console.log(err.message)
 		res.status(500).json({ msg: "Auction Item search error - search title" })
 	}
-});
+})
 
 //@route   GET api/item/id
 //@desc    Test route
 //@access  Public
-router.get('/id',async(req, res) => {
-    var id = req.query.id	
+router.get("/:id", async (req, res) => {
+	var id = req.params.id
 	try {
-        const item = await AuctionItem.findById(id);
-		res.json(item)		
-        //res.json({message: "Hello id " + id});
+		const item = await AuctionItem.findById(id)
+		res.json(item)
+		//res.json({message: "Hello id " + id});
 	} catch (err) {
 		console.log(err.message)
 		res.status(500).json({ msg: "Auction Item search error - id" })
 	}
-});
+})
 module.exports = router
