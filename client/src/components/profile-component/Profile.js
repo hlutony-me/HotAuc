5d2a971a88eb8b6574a5d9deb5d5a5cb04e79949
mengxi li <mengxi616@gmail.com>
2021-11-23 20:13:21 -0500

    dashboard with profile

---
+++ client/src/components/profile-component/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Profile.css";
+function Profile(){
+    return(
+        <div class="Profile">
+            <h2>Profile Information</h2>
+            <h4>Name:</h4>
+            <p>Nike</p>
+            <h4>Email:</h4>
+            <p>nike@gmail.com</p>
+        </div>
+
+    )
+}
+export default Profile
