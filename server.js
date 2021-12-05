f35c6c22e4cf17fb70d35ccc0e7ac3f4770af1ed
hanhan9334 <lhtony9334@126.com>
2021-12-04 23:15:53 -0500

    handle cors

--- server.js
+++ server.js
@@ -11,18 +11,38 @@ connectDB()
 //Init middleware
 app.use(express.json({ extended: false }))
 
-
-
 app.get("/", (req, res) => {
 	res.send("API running")
+})
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+	// Website you wish to allow to connect
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+
+	// Request methods you wish to allow
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
+	)
+
+	// Request headers you wish to allow
+	res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type")
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader("Access-Control-Allow-Credentials", true)
+
+	// Pass to next layer of middleware
+	next()
 })
 
 //Use Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use("/api/auth", require("./routes/api/auth"))
-app.use("/api/user",require('./routes/api/user'))
-app.use("/api/item",require('./routes/api/item'))
+app.use("/api/user", require("./routes/api/user"))
+app.use("/api/item", require("./routes/api/item"))
 
 const PORT = process.env.PORT || 5000
 
