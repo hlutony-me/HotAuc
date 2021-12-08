107c78a7ef84cd94baf0d4c2057cb381de0e2140
Tien Sang Nguyen <ntbs1798@gmail.com>
2021-12-08 16:10:39 +0700

    added edit-profile form

--- client/src/components/edit-dashboard-component/edit-profile.js
+++ client/src/components/edit-dashboard-component/edit-profile.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./Edit-Profile.css"
 
 function EditProfile() {
     return (
-			<div className="edit-profile">
-				edit profile
-            <Link to="/dashboard">go back to dash board</Link>
-			</div>
+		<div class="boxProfile">
+		<form action="">
+		<img id="profileImage" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt ="profile img" width="30px" height="30px"/>
+		<input type="text" id="userName" placeholder="User Name" name="" />
+		<input type="Email" id="Email" placeholder="Email ID" name="" />
+		<input type="password" id="Password" placeholder="Password" name="" />
+
+		<button class="buttonEdit" id="cancelBtn" type="reset">CANCEL</button>
+
+		<button class="buttonEdit">DONE</button>
+		</form>
+		</div>
 		)
 }
 
