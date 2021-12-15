3b5216040a4fa1a51b3e7c7245adfc5cc6f12e39
hanhan9334 <lhtony9334@126.com>
2021-12-15 15:43:19 -0500

    update endpoint

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,8 +3,8 @@ const router = express.Router()
 //Middleware
 const auth = require("../../middleware/auth")
 
-const User = require('../../models/user')
-const Item = require('../../models/auction_item')
+const User = require("../../models/user")
+const Item = require("../../models/auction_item")
 
 //@route   GET api/user
 //@desc    Test route
@@ -33,8 +33,16 @@ router.get("/:userId", auth, async (req, res) => {
 	}
 })
 
-router.put("/:userId", auth, async (req, res) => {
+router.put("/:userId", async (req, res) => {
 	var userId = req.params.userId
+	try {
+		const user = await User.findOneAndUpdate({ _id: userId }, req.body)
+		const respond = await User.findById(userId)
+		res.json(respond)
+	} catch (err) {
+		console.log(err.message)
+		res.status(500).json({ msg: "User not authenticated" })
+	}
 })
 
 module.exports = router
